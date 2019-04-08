72964d3becbd901cb578e3c4b0acba54834c0a3c
Yi Zuo <zuoyi@bu.edu>
2019-04-07 21:45:00 -0500

    preliminary descriptives

--- Analysis/R/DescriptiveStatistics.R
+++ Analysis/R/DescriptiveStatistics.R
@@ -2,3 +2,23 @@
 # Author: Yi Zuo
 # Created on 04/07/2019
 
+# read in data
+setwd("~/Google Drive File Stream/My Drive/Vanderbilt/Fun/FlightHistory/Analysis/R")
+data <- read.csv("../../Data/Raw data_2019_04_07.csv")
+
+dim(data)
+
+# top origin airport: BOS 15 times
+sort(table(data$Origin))
+
+# top destination airport: BOS 18 times
+sort(table(data$Destination))
+
+# top airline: B6 (16), HU (14)
+sort(table(data$Airline))
+
+# top aircraft: A320 and B787 (14 times, tie)
+sort(table(data$Aircraft))
+length(table(data$Aircraft))
+
+
